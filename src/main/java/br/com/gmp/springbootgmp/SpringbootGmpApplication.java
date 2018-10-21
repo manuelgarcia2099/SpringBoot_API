22f3d7fae6a5e9c08476adcc560be70b822d111a
Manuel Garcia <manuelgarcianp@gmail.com>
2018-10-21 13:42:31 -0300

    artefactos

---
+++ src/main/java/br/com/gmp/springbootgmp/SpringbootGmpApplication.java
@@ -0,0 +1,12 @@
+package br.com.gmp.springbootgmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGmpApplication.class, args);
+	}
+}
